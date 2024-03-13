a364827e64b7805625061dd69730d3007d404598
kanbaru-github <usupfv@gmail.com>
2024-03-13 22:36:05 +0900

    Add sandbox

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: '/suzuki-hiroaki-static/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        sandbox: 'sandbox.html',
+      }
+    }
+  }
 });
