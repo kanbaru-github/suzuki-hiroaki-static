959b56df3bd015844f210acef0b084bbbf14cfad
kanbaru-github <usupfv@gmail.com>
2024-03-22 10:36:25 +0900

    Response to review finding.

--- src/ts/modules/_counter.ts
+++ src/ts/modules/_counter.ts
@@ -1,33 +1,50 @@
 import goodClickedIcon from '/img/icon-good-clicked.gif';
 
+/**
+ * ローカルストレージのGoodカウントをリセット及びリロード
+ */
+const resetGoodCnt = () => {
+  const resetBtn = document.getElementById('js-resetGoodBtn');
+  resetBtn?.addEventListener('click', () => {
+    localStorage.removeItem('good-count');
+    location.reload();
+  });
+}
+
+/**
+ * Goodカウントをローカルストレージを用いて制御
+ * @param element - Goodボタン
+ */
 export const handleGoodBtn = (element: HTMLElement) => {
-  let counter = localStorage.getItem('good-count') ? parseInt(localStorage.getItem('good-count') || '0', 10) : 0;
+  const goodCnt = localStorage.getItem('good-count');
+  let counter = goodCnt ? parseInt(goodCnt, 10) : 0;
 
   const setupCnt = (cnt: number, isClick?: boolean) => {
     counter = cnt;
-    const spanElement = element?.querySelector('span');
+    const spanElement = element.querySelector('span');
     if (spanElement) {
       spanElement.textContent = counter.toString();
     }
 
-    const imgElement = element?.querySelector('img');
+    const imgElement = element.querySelector('img');
     if (imgElement && isClick) {
       imgElement.src = goodClickedIcon;
     }
 
     localStorage.setItem('good-count', counter.toString());
 
-    element.classList.add('is-clicked');
-    setTimeout(() => {
-      element.classList.remove('is-clicked');
-    }, 1000);
+    if (!element.classList.contains('is-clicked')) {
+      element.classList.add('is-clicked');
+      setTimeout(() => {
+        element.classList.remove('is-clicked');
+      }, 1000);
+    }
   }
-  element.addEventListener('click', () => setupCnt(counter + 1, true));
-  setupCnt(counter);
 
-  const resetBtn = document.getElementById('js-resetGoodBtn');
-  resetBtn?.addEventListener('click', () => {
-    localStorage.removeItem('good-count');
-    location.reload();
-  });
+  if (element) {
+    element.addEventListener('click', () => setupCnt(counter + 1, true));
+    setupCnt(counter);
+  }
+
+  resetGoodCnt();
 }
