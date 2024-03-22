4c423e5498edb2f8b9c6814c9a61d3eae2468622
kanbaru-github <usupfv@gmail.com>
2024-03-22 10:47:44 +0900

    Updates

---
+++ assets/sandbox-Bzf7fj5P.js
@@ -0,0 +1,7 @@
+import"./main-D1_x8SmW.js";document.querySelector("#app").innerHTML=`
+  <div>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+`;
--- assets/sandbox-d2LdiI94.js
+++
@@ -1,7 +0,0 @@
-import"./main-CBVWv15F.js";function u(e){let t=0;const n=o=>{t=o,e.innerHTML=`count is ${t}`};e.addEventListener("click",()=>n(t+1)),n(0)}document.querySelector("#app").innerHTML=`
-  <div>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`;u(document.querySelector("#counter"));
