0d4e527dd04b520ff4c3f7e20bccd8c066e455a1
kanbaru-github <usupfv@gmail.com>
2024-03-22 15:49:01 +0900

    Updates

--- assets/sandbox-DmMVysa4.js
+++ assets/sandbox-DmMVysa4.js
@@ -1,4 +1,4 @@
-import"./main-D1_x8SmW.js";document.querySelector("#app").innerHTML=`
+import"./main-CHEafxP5.js";document.querySelector("#app").innerHTML=`
   <div>
     <div class="card">
       <button id="counter" type="button"></button>
